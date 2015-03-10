8c8905671c8191766ae043fe81ba608b6a1b11e7
Sevy11 <michaelsevy@gmail.com>
2015-03-10 14:03:48 -0500

    Initial Commit

---
+++ Wordplay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wordplay
+//
+//  Created by Michael Sevy on 3/10/15.
+//  Copyright (c) 2015 Michael Sevy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Wordplay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wordplay
+//
+//  Created by Michael Sevy on 3/10/15.
+//  Copyright (c) 2015 Michael Sevy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
